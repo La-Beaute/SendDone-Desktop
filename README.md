175b02406495ffd8c7581db73d11761a5d4ba2f8
dlguswo333 <dlguswo333@skku.edu>
2021-04-13 16:22:07 +0900

    improve networking

--- README.md
+++ README.md
@@ -11,10 +11,46 @@ npm run electron
 
 ## How to Build in Production
 ```bash
+# Option 1.
 # Run react build
-npm start
+npm build
 # electron-builder will build into executables or installers. Refer to its document for detail.
 npx electron-builder --win --x64
-# Or from npm script.
+
+# Option 2.
+# Execute above with one npm script.
 npm run builder
 ```
+
+## networking
+`src/networking.js` is the underline implementation of TCP networks and file write and read.<br>
+Receiver always opens a server socket, and sender always connects to the server socket as a client.<br>
+Why? Because that is easier and more natural than the opposite way.<br>
+Think of a mouth and an ear. Mouth always initiate communication, and ear responds to the mouth.<br> 
+To communicate at any arbitrary time, ear has to be open always, while mouth does not have to be.
+<br>
+
+### Send
+Sender connect to receiver and sends the following data first.
+```text
+SendDone\r\n
+0.1.0\r\n
+type:send\r\n
+length:4\r\n
+name:file_1\r\n
+type:file\r\n
+name:file_2\r\n
+type:file\r\n
+name:sub_directory\r\n
+type:directory\r\n
+name:sub_directory/file_3\r\n
+type:file\r\n\r\n
+```
+Next, if receiver approves seeing the list, sender sends each element.<br>
+If the element type is file, first send the file metadata.
+```text
+name:file_1\r\n
+type:file\r\n
+size:1000\r\n\r\n
+```
+Then, sender waits for receiver to send `ok` sign. This is to handle user inputs and to distinguish element's metadata and actual data.<br>
--- src/networking.js
+++ src/networking.js
@@ -3,13 +3,14 @@ const net = require('net');
 const fs = require('fs').promises;
 const path = require('path');
 const port = 8531;
+const chunkSize = 1 * 1024 * 1024;
 var serverSocket = null;
 var socketBusy = false;
 
-// Get my LAN ip and netmask array.
-// Not that it returns an array because there could be
-// multiple network interfaces with local ip.
-// Each array is a dict: { name, ip, netmask }
+/**
+ * Return an array of dictionary each looks like: { name, ip, netmask }.
+ * @returns {Array.<{name:String, ip:String, netmask:String}>} Array of networks.
+ */
 const getMyNetworks = () => {
     var array = [];
     const interfaces = os.networkInterfaces();
@@ -28,6 +29,11 @@ const getMyNetworks = () => {
     return array;
 }
 
+/**
+ * Initialize the server socket with the ip address.
+ * Note that the port number is fixed thus cannot be changed.
+ * @param {String} ip address. 
+ */
 const initServerSocket = (ip) => {
     if (!ip) {
         // ip is not set.
@@ -65,14 +71,20 @@ const initServerSocket = (ip) => {
             console.log(data);
         });
     });
-
     serverSocket.listen(port, ip);
 }
 
+/**
+ * Return whether the server socket is not null and it is listening.
+ * @returns {bool}
+ */
 const isServerSocketListening = () => {
     return serverSocket && serverSocket.listening;
 }
 
+/**
+ * Close the server socket.
+ */
 const closeServerSocket = () => {
     if (serverSocket) {
         serverSocket.close();
@@ -80,31 +92,91 @@ const closeServerSocket = () => {
     }
 }
 
-// The parameter must be absolute.
-const sendFile = async (targetPath) => {
-    if (!path.isAbsolute())
-        return false;
-    var offset = 0;
-    var chunk = null;
-    const name = path.basename();
-    const stat = await fs.stat(targetPath).catch(() => {
-        // Cannot read file.
-        return false;
+const send = async (sendArray, receiverIp) => {
+    var clientSocket = net.createConnection(port, receiverIp);
+    // TODO send metadata and elements array.
+    clientSocket.on('connect', async () => {
+        console.log('total ' + sendArray.length);
+        for (let i = 0; i < sendArray.length; ++i) {
+            await sendElement(sendArray[i], clientSocket);
+        }
     });
-    if (stat.isFile()) {
-
-    }
-    else if (stat.isDirectory()) {
-
-    }
-    else {
-        // What the hell?
-    }
-
 }
 
-const receiveFile = (downloadPath, name) => {
+/**
+ * Send the target element to socket opponent.
+ * This shall not be directly called from UI.
+ * @param {String} targetPath absolute path of the target.
+ * @param {net.Socket} socket connected socket.
+ * @returns {Promise<bool>}
+ */
+const sendElement = (targetPath, socket) => {
+    return new Promise(async (resolve, reject) => {
+        if (!path.isAbsolute(targetPath))
+            reject(false);
+        const stat = await fs.stat(targetPath).catch(() => {
+            // Cannot read file.
+            reject(false);
+        });
+        if (stat.isFile()) {
+            const header = createFileHeader(stat);
+            socket.write(header, 'utf-8');
+            let chunkInd = 0;
+            const handle = await fs.open(targetPath);
+            const onData = async (data) => {
+                if (data.toString('utf8') === 'ok') {
+                    let chunk = new Buffer.alloc(chunkSize);
+                    const ret = await handle.read(chunk, 0, chunkSize, null);
+                    if (ret.bytesRead === 0) {
+                        // EOF reached. Close the file.
+                        await handle.close();
+                        console.log('EOF');
+                        // Remove listener.
+                        socket.removeListener('data', onData);
+                        resolve(true);
+                    }
+                    else {
+                        socket.write(chunk, () => {
+                            console.log('chunk ' + (chunkInd++) + 'sent complete');
+                        });
+                    }
+                }
+            };
+            // Add listener.
+            socket.on('data', onData);
+        }
+        else if (stat.isDirectory()) {
+            // TODO implement.
+            resolve(true);
+        }
+        else {
+            // What the hell?
+            resolve(false);
+        }
+    });
 
 }
+/**
+ * Receive one element from sender.
+ * Note that this shall not be directly called from UI.
+ * @param {String} downloadPath the absolute path set by user to download files into.
+ */
+const recvElement = (downloadPath) => {
+    // TODO implement
+}
 
-module.exports = { getMyNetworks, initServerSocket, isServerSocketListening, closeServerSocket };
+/**
+ * 
+ * @param {fs.Stats} stat fs.Stats object of the element.
+ * @returns {String} 
+ */
+const createFileHeader = (stat) => {
+    const name = path.basename(targetPath);
+    const size = stat.size;
+    const nameHeader = 'name:' + name + '\r\n';
+    const typeHeader = 'type:file\r\n';
+    const sizeHeader = 'size:' + size + '\r\n\r\n';
+    return nameHeader + typeHeader + sizeHeader;
+}
+
+module.exports = { getMyNetworks, initServerSocket, isServerSocketListening, closeServerSocket, send };
